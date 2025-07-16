295226c3abdeb1d43b485c6327a55419adedea0a
arnab-098 <arnab.chattopadhyay777@gmail.com>
2025-07-16 15:08:54 +0530

    week 5 assignments finished

---
+++ Week 5/Week 5_2. Microservices with API gateway/Creating Microservices for account and loan/Source Code/account service/Account.java
@@ -0,0 +1,37 @@
+package com.cognizant.account.model;
+
+public class Account {
+    private String number;
+    private String type;
+    private int balance;
+
+    public Account(String number, String type, int balance) {
+        this.number = number;
+        this.type = type;
+        this.balance = balance;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
